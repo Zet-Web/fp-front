09764017fb777088c7d5910808610ed3af4b2688
global50 <117812963+global50@users.noreply.github.com>
2025-10-25 16:07:40 +0300

    Updated contacts-section.tsx

--- apps/profile/src/components/contacts-section.tsx
+++ apps/profile/src/components/contacts-section.tsx
@@ -451,7 +451,7 @@ export function ContactsSection({ user, isOwnProfile, isEditing, onUpdateProfile
               onClick={() => setShowAddForm(true)}
             >
               <Plus className="w-4 h-4 mr-1" />
-              Add Contact
+              Add
             </Button>
           )}
         </CardTitle>
