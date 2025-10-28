25da35b6ab8e3122850638a948b053276ecef406
global50 <117812963+global50@users.noreply.github.com>
2025-10-28 17:11:36 +0300

    Updated FeedFilters.tsx

--- shared-src/components/FeedFilters.tsx
+++ shared-src/components/FeedFilters.tsx
@@ -86,7 +86,7 @@ export function FeedFilters({
             </Select>
           </div>
 
-          <div className="flex-2">
+          <div className="flex-1">
             <FeedLocationDropdown
               country={location.country}
               city={location.city}
