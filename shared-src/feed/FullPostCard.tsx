9859c5536a483ff7b796874096eef4c8238286fd
global50 <117812963+global50@users.noreply.github.com>
2025-11-21 04:51:16 +0300

    Updated FullPostCard.tsx

--- shared-src/feed/FullPostCard.tsx
+++ shared-src/feed/FullPostCard.tsx
@@ -223,7 +223,7 @@ export function FullPostCard({
               )}
             </div>
 
-            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight">
               {title}
             </h1>
 
