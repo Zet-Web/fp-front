b7e0cd545b6ab599773b68319c1b1a59d22232a4
global50 <117812963+global50@users.noreply.github.com>
2025-11-22 22:45:18 +0300

    Updated LocationSelector.tsx

--- apps/profile/src/components/LocationSelector.tsx
+++ apps/profile/src/components/LocationSelector.tsx
@@ -258,7 +258,7 @@ export function LocationSelector({
               <TabsContent value="country" className="mt-0">
                 <Command shouldFilter={false}>
                   <CommandInput
-                    placeholder="Search countries..."
+                    placeholder="Поиск..."
                     value={searchQuery}
                     onValueChange={setSearchQuery}
                   />
