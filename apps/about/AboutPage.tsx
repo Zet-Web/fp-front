223a365e9804c0cb64fb061f5453970b4bd4b8b4
global50 <117812963+global50@users.noreply.github.com>
2025-11-23 10:28:29 +0300

    Updated AboutPage.tsx

--- apps/about/AboutPage.tsx
+++ apps/about/AboutPage.tsx
@@ -200,7 +200,7 @@ export function AboutPage() {
 
             <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-3 mt-4">
               <div className="p-3 md:p-4 rounded-lg bg-gradient-to-br from-blue-500/5 to-blue-600/5 border border-blue-500/10 hover:border-blue-500/30 hover:shadow-md transition-all">
-                <h4 className="font-semibold text-sm md:text-base text-center">Каталог юридических инструментов</h4>
+                <h4 className="font-semibold text-sm md:text-base text-center">Юридические сервисы</h4>
               </div>
 
               <div className="p-3 md:p-4 rounded-lg bg-gradient-to-br from-blue-500/5 to-blue-600/5 border border-blue-500/10 hover:border-blue-500/30 hover:shadow-md transition-all">
