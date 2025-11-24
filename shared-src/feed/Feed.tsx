14421b7ac4456b30ef8c7351dfe75bbf204d8873
global50 <117812963+global50@users.noreply.github.com>
2025-11-24 12:58:12 +0300

    Updated Feed.tsx

--- shared-src/feed/Feed.tsx
+++ shared-src/feed/Feed.tsx
@@ -144,7 +144,7 @@ export function Feed({
       controller.abort();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page, filters, itemsPerPage, hasMore, shouldShowAuthPrompt, filterByUsername]);
+  }, [page, filters, itemsPerPage, hasMore, shouldShowAuthPrompt]);
 
   const attachObserver = useCallback(
     (node: HTMLDivElement | null) => {
